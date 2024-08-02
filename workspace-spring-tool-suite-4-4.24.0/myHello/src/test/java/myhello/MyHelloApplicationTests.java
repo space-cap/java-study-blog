14c4a8d254066e19704f60464196f246a59ae1ca
space-cap <hong.git@yahoo.com>
2024-08-02 14:35:55 +0900

    hello 초기 파일

---
+++ workspace-spring-tool-suite-4-4.24.0/myHello/src/test/java/myhello/MyHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package myhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
