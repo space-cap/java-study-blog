9cdd62f5c7e16507577e896ce8f31c436a486ca6
space-cap <hong.git@yahoo.com>
2024-08-05 21:28:16 +0900

    test code

---
+++ hello/src/test/java/com/ezlevup/hello/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.ezlevup.hello.repository;
+
+import com.ezlevup.hello.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findByid(member.getId()).get();
+        //System.out.println("result = " + (result == member));
+
+        //Assertions.assertEquals(result, member);
+
+        //Assertions.assertThat
+
+    }
+
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("sprint1").get();
+
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> list = new ArrayList<>();
+
+    }
+
+}
