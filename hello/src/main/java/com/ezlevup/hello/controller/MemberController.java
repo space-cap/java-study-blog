714182eaeab9e8f02ab6dcddaf5c0788ea0a9205
space-cap <hong.git@yahoo.com>
2024-08-06 16:32:41 +0900

    member test

---
+++ hello/src/main/java/com/ezlevup/hello/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ezlevup.hello.controller;
+
+public class MemberController {
+}
---
+++ hello/src/test/java/com/ezlevup/hello/service/MemberServiceTest.java
@@ -0,0 +1,73 @@
+package com.ezlevup.hello.service;
+
+import com.ezlevup.hello.domain.Member;
+import com.ezlevup.hello.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        //Assertions.assertEquals();
+        //Member findMember = memberService.findOne(saveId).get();
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        assertThrows(IllegalAccessError.class, ()-> memberService)
+
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalAccessError e) {
+//
+//        }
+
+        // then
+    }
+}
