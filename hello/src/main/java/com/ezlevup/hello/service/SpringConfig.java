384d5f17cd051b0a4a085bd3c215418a4930a9c7
space-cap <hong.git@yahoo.com>
2024-08-06 17:03:23 +0900

    spring controller

--- hello/src/main/java/com/ezlevup/hello/service/SpringConfig.java
+++ hello/src/main/java/com/ezlevup/hello/service/SpringConfig.java
@@ -1,5 +1,20 @@
 package com.ezlevup.hello.service;
 
+import com.ezlevup.hello.repository.MemberRepository;
+import com.ezlevup.hello.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
-    
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
