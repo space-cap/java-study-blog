2a1902b55ebd0ffdc74b0135bffab2492cbacac8
space-cap <hong.git@yahoo.com>
2024-08-06 21:22:04 +0900

    member 등록

--- hello/src/main/java/com/ezlevup/hello/controller/HomeController.java
+++ hello/src/main/java/com/ezlevup/hello/controller/HomeController.java
@@ -1,13 +1,37 @@
 package com.ezlevup.hello.controller;
 
+import com.ezlevup.hello.domain.Member;
+import com.ezlevup.hello.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    private final MemberService memberService;
+
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
