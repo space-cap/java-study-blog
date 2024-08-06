45c83c29aa8b278c3842078faa6a84459fea8bf1
space-cap <hong.git@yahoo.com>
2024-08-06 14:41:04 +0900

    회원 서비스

---
+++ hello/src/main/java/com/ezlevup/hello/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.ezlevup.hello.service;
+
+import com.ezlevup.hello.domain.Member;
+import com.ezlevup.hello.repository.MemberRepository;
+import com.ezlevup.hello.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findByid(memberId);
+    }
+
+}
