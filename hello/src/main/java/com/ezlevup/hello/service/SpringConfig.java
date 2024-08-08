cc7d0bafab8688fa77941d926179c2726048c140
space-cap <hong.git@yahoo.com>
2024-08-08 17:52:47 +0900

    JdbcTemplateMemberRepository

--- hello/src/main/java/com/ezlevup/hello/service/SpringConfig.java
+++ hello/src/main/java/com/ezlevup/hello/service/SpringConfig.java
@@ -1,12 +1,24 @@
 package com.ezlevup.hello.service;
 
+import com.ezlevup.hello.repository.JdbcTemplateMemberRepository;
 import com.ezlevup.hello.repository.MemberRepository;
 import com.ezlevup.hello.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
