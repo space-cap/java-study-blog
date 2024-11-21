934e6ab38b6bbb8dec60ea9d4665e640b169c42e
COCO <hong.git@yahoo.com>
2024-11-21 17:21:14 +0900

    Update Answer.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/Answer.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -1,5 +1,26 @@
 package com.mysite.sbb;
 
-public class Answer {
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate; 
+
+    private Question question;  
 }
