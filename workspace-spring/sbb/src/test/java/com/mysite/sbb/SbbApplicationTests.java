8c7122ff68f2c93911d8de68de4e2a5f128f089e
COCO <hong.git@yahoo.com>
2024-11-21 17:47:53 +0900

    test

--- workspace-spring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ workspace-spring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,8 +2,6 @@ package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,10 +15,8 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
     }
 }
