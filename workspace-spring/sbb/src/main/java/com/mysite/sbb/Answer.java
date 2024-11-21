a97b29e19719a03fecdf071cbb69a2c2f2aa6b34
COCO <hong.git@yahoo.com>
2024-11-21 17:22:10 +0900

    Update Answer.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/Answer.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,5 +23,6 @@ public class Answer {
 
     private LocalDateTime createDate; 
 
+    @ManyToOne
     private Question question;  
 }
