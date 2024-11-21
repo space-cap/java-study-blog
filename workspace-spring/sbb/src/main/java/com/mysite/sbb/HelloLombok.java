353cab75bcee80ba3eb61520845f460fa444e2bb
COCO <hong.git@yahoo.com>
2024-11-21 16:32:18 +0900

    main

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,20 +1,16 @@
-package com.mysite.sbb;
+package com.mysite.sbb; 
+import lombok.Getter; 
+import lombok.RequiredArgsConstructor; 
 
-import lombok.Getter;
-import lombok.Setter;
+@RequiredArgsConstructor 
+@Getter 
+public class HelloLombok { 
+    private final String hello; 
+    private final int lombok; 
 
-@Getter
-@Setter
-public class HelloLombok {
-    private String hello;
-    private int lombok;
-
-    public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
-
-        System.out.println(helloLombok.getHello());
-        System.out.println(helloLombok.getLombok());
-    }
+    public static void main(String[] args) { 
+        HelloLombok helloLombok = new HelloLombok("헬로", 5); 
+        System.out.println(helloLombok.getHello()); 
+        System.out.println(helloLombok.getLombok()); 
+    } 
 }
