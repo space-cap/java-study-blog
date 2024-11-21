dcf2fceb6b14bf0392a143a5b215910a357eb51f
COCO <hong.git@yahoo.com>
2024-11-21 17:52:20 +0900

    Update SbbApplicationTests.java

--- workspace-spring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ workspace-spring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,8 +1,8 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +17,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
     }
 }
