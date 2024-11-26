fd5b5f3fe188c1b3afabd0e9922fe347681c26ba
COCO <hong.git@yahoo.com>
2024-11-26 16:15:31 +0900

    signup

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/user/UserController.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -34,8 +36,18 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(), 
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
+        try {
+            userService.create(userCreateForm.getUsername(), 
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
