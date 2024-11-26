cd0ef130dad66e6a3e4d337eef73aa952cfba0f9
COCO <hong.git@yahoo.com>
2024-11-26 16:08:54 +0900

    Update UserService.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/user/UserService.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,6 +1,6 @@
 package com.mysite.sbb.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,12 +10,12 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
