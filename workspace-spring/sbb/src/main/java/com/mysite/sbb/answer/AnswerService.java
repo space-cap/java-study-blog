617474ea29b0a7892d9ffe99882dfa9bd1de4994
COCO <hong.git@yahoo.com>
2024-11-26 16:36:42 +0900

    Update AnswerService.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import com.mysite.sbb.user.SiteUser;
 
 @RequiredArgsConstructor
 @Service
@@ -13,11 +14,12 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
 }
