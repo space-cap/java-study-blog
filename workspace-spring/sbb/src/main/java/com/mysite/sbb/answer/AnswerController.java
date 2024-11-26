b3c67b9d8b25ff1c288340a1f73a1e1c098e0c16
COCO <hong.git@yahoo.com>
2024-11-26 16:37:48 +0900

    Update AnswerController.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import java.security.Principal;
-
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 
 
 @RequestMapping("/answer")
@@ -22,16 +23,18 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
     
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, 
             @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
