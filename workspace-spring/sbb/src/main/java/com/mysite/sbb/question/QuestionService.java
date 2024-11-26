99df47eb96b37a7f7f3a01da8f65d8c66d9a8666
COCO <hong.git@yahoo.com>
2024-11-26 15:46:44 +0900

    Update QuestionService.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,17 +1,20 @@
 package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
+
 
 
 @RequiredArgsConstructor
@@ -42,7 +45,9 @@ public class QuestionService {
     }
     
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 }
