16a428c6e0fce3b35efbbd01d21a1ed292bb6c1d
COCO <hong.git@yahoo.com>
2024-11-26 13:37:32 +0900

    Update QuestionController.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,5 +36,11 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+    
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+        // TODO 질문을 저장한다.
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
