755fa2f88255505ad8eb7fc9c9f390e611555e41
COCO <hong.git@yahoo.com>
2024-11-26 16:38:39 +0900

    Update QuestionService.java

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
-
+import com.mysite.sbb.user.SiteUser;
 
 
 @RequiredArgsConstructor
@@ -36,11 +36,12 @@ public class QuestionService {
         }
     }
     
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
     }
     
