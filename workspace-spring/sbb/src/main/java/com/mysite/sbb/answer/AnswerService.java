d423c2c77aa4660dd5751c485b0e4e376ef07e00
COCO <hong.git@yahoo.com>
2024-11-26 18:51:59 +0900

    detail

--- workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ workspace-spring/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import com.mysite.sbb.user.SiteUser;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +23,26 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+    
+    
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+    
+    
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
