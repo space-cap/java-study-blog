1c37d0750e9b3ab48f7e2b4f98ee16a43fa65042
COCO <hong.git@yahoo.com>
2025-03-28 00:13:03 +0900

    casting

---
+++ java_standard_3rd/Hello/src/ch02/TypeCastingEx2.java
@@ -0,0 +1,19 @@
+package ch02;
+
+// 형번환(casting)
+// 형변환이란, 변수 또는 상수의 타입을 다른 타입으로 변화하는 것
+
+
+
+
+public class TypeCastingEx2 {
+    public static void main(String[] args) {
+
+        // (타입)피연산자
+        double d = 85.4;
+        int score = (int)d;
+
+
+
+    }
+}
