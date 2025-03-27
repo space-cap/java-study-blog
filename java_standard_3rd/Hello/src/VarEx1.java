3812b9f8d50d853eba5332a3365e1f3d14a50ea0
COCO <hong.git@yahoo.com>
2025-03-27 12:18:54 +0900

    init

---
+++ java_standard_3rd/Hello/src/VarEx1.java
@@ -0,0 +1,17 @@
+
+// 변수란, 단 하나의 값을 저장할 수 있는 메모리 공간.
+// 변수의 초기화란, 변수를 사용하기 전에 처음으로 값을 저장하는 것.
+
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int age = 0; // age 변수명, int 타입, 0으로 초기화,
+        System.out.println("age: " + age);
+
+        age = age  + 1;
+        System.out.println("age: " + age);
+
+
+
+    }
+}
