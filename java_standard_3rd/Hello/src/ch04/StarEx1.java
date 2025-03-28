a887f61dd40f45d8c4214642ecdbb5d016232dbf
COCO <hong.git@yahoo.com>
2025-03-28 15:00:00 +0900

    star

--- java_standard_3rd/Hello/src/ch04/StarEx1.java
+++ java_standard_3rd/Hello/src/ch04/StarEx1.java
@@ -4,12 +4,12 @@ public class StarEx1 {
     public static void main(String[] args) {
         // 별찍기
         /*
-        *****
-        *****
-        *****
-        *****
-        *****
-        */
+         *****
+         *****
+         *****
+         *****
+         *****
+         */
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
--- java_standard_3rd/Hello/src/ch04/StarEx10.java
+++ java_standard_3rd/Hello/src/ch04/StarEx10.java
@@ -4,7 +4,7 @@ public class StarEx10 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                if (i==j || j==1 || j==5 || i+j==6 || i==3) {
+                if (i == j || j == 1 || j == 5 || i + j == 6 || i == 3) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
