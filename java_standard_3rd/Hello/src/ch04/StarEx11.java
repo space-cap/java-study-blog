b3125b9cafcf1ce0fe0065473b36d8c2fd71d351
COCO <hong.git@yahoo.com>
2025-03-28 14:56:29 +0900

    star

--- java_standard_3rd/Hello/src/ch04/StarEx11.java
+++ java_standard_3rd/Hello/src/ch04/StarEx11.java
@@ -1,20 +1,14 @@
 package ch04;
 
 public class StarEx11 {
-    /*
-     *   *
-     ** **
-     *****
-     ** **
-     *   *
-     */
+
 
     public static void main(String[] args) {
 
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                if (i==j || j==1 || j==5 || i+j==6 || i==3) {
+                if (i == j || i + j == 6) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
--- java_standard_3rd/Hello/src/ch04/StarEx4.java
+++ java_standard_3rd/Hello/src/ch04/StarEx4.java
@@ -9,5 +9,16 @@ public class StarEx4 {
         System.out.println("*   *");
         System.out.println();
 
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == j || i + j == 6) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
