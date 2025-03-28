00d75b1d68d8938dedd821e1228e9190198197c3
COCO <hong.git@yahoo.com>
2025-03-28 14:53:49 +0900

    Update StarEx4.java

--- java_standard_3rd/Hello/src/ch04/StarEx4.java
+++ java_standard_3rd/Hello/src/ch04/StarEx4.java
@@ -2,39 +2,12 @@ package ch04;
 
 public class StarEx4 {
     public static void main(String[] args) {
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.print("*");
+        System.out.println("*   *");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*   *");
         System.out.println();
 
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.println();
-
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.println();
-
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.println();
-
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.print(" ");
-        System.out.print("*");
-        System.out.println();
     }
 }
