4ba845536bf38564b5698a9239159c49c8482dcb
COCO <hong.git@yahoo.com>
2025-04-01 18:05:42 +0900

    Update ex01.java

--- java_standard_3rd/Hello/src/ch05/ex01.java
+++ java_standard_3rd/Hello/src/ch05/ex01.java
@@ -13,14 +13,21 @@ public class ex01 {
         int value = (int) (Math.random() * 6) + 1;
         //System.out.println(value);
 
-        // 0<=x<=10, 0<=y<=10
-        for (int x = 0; x <= 10; x++) {
-            for (int y = 0; y <= 10; y++) {
-                if (2 * x + 4 * y == 10) {
-                    System.out.println("x: " + x + ", y: " + y);
-                }
-            }
+
+        int num1 = 1;
+        int num2 = 1;
+        int num3 = 0;
+
+        System.out.print(num1);
+        System.out.print(num2);
+        for (int i = 0; i < 8; i++) {
+            num3 = num1 + num2;
+            System.out.print(num3 + ",");
+            num1 = num2;
+            num2 = num3;
         }
+
 
     }
 }
+
