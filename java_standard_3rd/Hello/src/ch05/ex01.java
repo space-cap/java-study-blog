209d4cbd80101ff55f3bd39cdcc25fd96800b508
COCO <hong.git@yahoo.com>
2025-04-01 16:22:59 +0900

    ex

--- java_standard_3rd/Hello/src/ch05/ex01.java
+++ java_standard_3rd/Hello/src/ch05/ex01.java
@@ -1,8 +1,13 @@
 package ch05;
 
 public class ex01 {
-    public static void main(String[] args) {
-        int i = Integer.MAX_VALUE;
-        int[] score = new int[Integer.MAX_VALUE];
+    static public void main(String[] args) {
+        // int i = Integer.MAX_VALUE;
+        // int[] score = new int[Integer.MAX_VALUE];
+
+
+        char ch = 'z';
+        boolean result = 'A' <= ch && ch <= 'Z' || 'a' <= ch && ch <= 'z' || '0' <= ch && ch <= '9';
+        System.out.println(result);
     }
 }
--- java_standard_3rd/Hello/src/ch05/quiz_2.java
+++ java_standard_3rd/Hello/src/ch05/quiz_2.java
@@ -17,7 +17,7 @@ public class quiz_2 {
         }
 
         // 정렬
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
