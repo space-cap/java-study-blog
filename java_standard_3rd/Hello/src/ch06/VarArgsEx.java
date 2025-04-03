59152721c8006a194f336d58f8446fd47638193d
COCO <hong.git@yahoo.com>
2025-04-03 14:36:58 +0900

    ch06

--- java_standard_3rd/Hello/src/ch06/VarArgsEx.java
+++ java_standard_3rd/Hello/src/ch06/VarArgsEx.java
@@ -3,6 +3,7 @@ package ch06;
 public class VarArgsEx {
     public static void main(String[] args) {
         String[] strArr = {"100", "200", "300"};
+        String[] strArr2 = new String[]{"1", "2", "3"};
 
         System.out.println(concatenate("", "100", "200", "300"));
         System.out.println(concatenate("-", strArr));
