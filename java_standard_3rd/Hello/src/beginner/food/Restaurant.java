4e2d327c3baecc3c70174c21b7498d6cdc6840cb
COCO <hong.git@yahoo.com>
2025-04-23 23:54:55 +0900

    Update Restaurant.java

--- java_standard_3rd/Hello/src/beginner/food/Restaurant.java
+++ java_standard_3rd/Hello/src/beginner/food/Restaurant.java
@@ -7,24 +7,18 @@ public class Restaurant {
     List<Food> foods = new ArrayList<>();
 
     void addMenu(Food food) {
-        /*if (foods.stream().noneMatch(f -> f.getName().equals(food.getName()))) {
-            foods.add(food);
-        }*/
-
-        for(var f : foods) {
-            if(f.getName().equals(food.getName())) { // ==는 객체의 주소(참조)를 비교
+        for (var f : foods) {
+            if (f.getName().equals(food.getName())) { // ==는 객체의 주소(참조)를 비교
                 return;
             }
         }
         foods.add(food);
-
-        /*if (!foods.contains(food)) {
-            foods.add(food);
-        }*/
     }
 
     void displayMenu() {
-        //System.out.println("저희 음식점의 메뉴는 " + food.getName() + " 입니다.");
-        //System.out.println("가격은 " + food.getPrice() + "원 입니다.");
+        foods.forEach(f -> {
+            System.out.println("저희 음식점의 메뉴는 " + f.getName() + " 입니다.");
+            System.out.println("가격은 " + f.getPrice() + "원 입니다.");
+        });
     }
 }
