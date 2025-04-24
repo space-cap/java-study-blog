7273da5052e025aeb614919b12a6ce9b47b16857
COCO <hong.git@yahoo.com>
2025-04-24 17:39:02 +0900

    Update Restaurant.java

--- java_standard_3rd/Hello/src/beginner/food/Restaurant.java
+++ java_standard_3rd/Hello/src/beginner/food/Restaurant.java
@@ -1,24 +1,32 @@
 package beginner.food;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Restaurant {
-    List<Food> foods = new ArrayList<>();
+    ///List<Food> foods = new ArrayList<>();
+
+    Map<String, Food> foods = new HashMap<>();
 
     void addMenu(Food food) {
-        for (var f : foods) {
-            if (f.getName().equals(food.getName())) { // ==는 객체의 주소(참조)를 비교
-                return;
-            }
+//        for (var f : foods) {
+//            if (f.getName().equals(food.getName())) { // ==는 객체의 주소(참조)를 비교
+//                return;
+//            }
+//        }
+//        foods.add(food);
+
+        if(!foods.containsKey(food.getName())) {
+            foods.put(food.getName(), food);
         }
-        foods.add(food);
     }
 
     void displayMenu() {
-        foods.forEach(f -> {
-            System.out.println("저희 음식점의 메뉴는 " + f.getName() + " 입니다.");
-            System.out.println("가격은 " + f.getPrice() + "원 입니다.");
+        foods.forEach((k,v) -> {
+            System.out.println("저희 음식점의 메뉴는 " + v.getName() + " 입니다.");
+            System.out.println("가격은 " + v.getPrice() + "원 입니다.");
         });
     }
 }
