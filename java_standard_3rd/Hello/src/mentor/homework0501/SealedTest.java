31799acc2b38ae9e1ee94581b13ff4bc217968a6
COCO <hong.git@yahoo.com>
2025-05-02 03:31:23 +0900

    Update SealedTest.java

--- java_standard_3rd/Hello/src/mentor/homework0501/SealedTest.java
+++ java_standard_3rd/Hello/src/mentor/homework0501/SealedTest.java
@@ -1,5 +1,24 @@
 package mentor.homework0501;
 
+
+// `Join`는 오직 `EquiJoin`, `NonEquiJoin`, `OuterJoin`만 상속할 수 있습니다.
+sealed class Join permits EquiJoin, NonEquiJoin, OuterJoin { }
+
+// `EquiJoin`은 더 이상 상속될 수 없다.
+final class EquiJoin extends Join { }
+
+// `NonEquiJoin`는 자유롭게 상속될 수 있다.
+non-sealed class NonEquiJoin extends Join { }
+
+// `OuterJoin`은 다시 한 번 상속 제한이 가능합니다.
+sealed class OuterJoin extends Join permits LeftOuterJoin, RightOuterJoin { }
+
+final class LeftOuterJoin extends OuterJoin { }
+final class RightOuterJoin extends OuterJoin { }
+
+
 public class SealedTest {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, Sealed");
+    }
 }
